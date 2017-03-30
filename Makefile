c899f2c8633732158f78a5c092d01067be470ae3
Kan Bok-Hua <mhc431@nyu.edu>
2017-03-29 22:57:34 -0400

    debug complete

--- Makefile
+++ Makefile
@@ -1,24 +1,11 @@
 CC=mpicc
 FLAGS=-O3
-EXECS=sr sr2 sr2_nonblocking pingpong pingpong_array pingpong_nonblocking
+EXECS= int_ring
 
 all: ${EXECS}
 
-sr: sr.c
-	${CC} ${FLAGS} $^ -o sr
-
-sr2: sr2.c
-	${CC} ${FLAGS} $^ -o sr2
-
-sr2_nonblocking: sr2_nonblocking.c
-	${CC} ${FLAGS} $^ -o sr2_nonblocking
-
-pingpong: pingpong.c
-	${CC} ${FLAGS} $^ -o pingpong
-
-pingpong_array: pingpong_array.c
-	${CC} ${FLAGS} $^ -o pingpong_array
-
+int_ring: int_ring.c
+	${CC} ${FLAGS} $^ -o int_ring
 
 clean:
 	rm -f ${EXECS}
--- int_ring.c
+++ int_ring.c
@@ -8,35 +8,48 @@
 
 int main( int argc, char *argv[])
 {
-  int rank, tag, source, destination;
+  int rank, tag, source, destination, m;
   MPI_Status status;
-
-  char hostname[1024];
-  gethostname(hostname, 1024);
 
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  if (argc != 2) {
+    fprintf(stderr, "Function needs the number of passes as input arguments!\n");
+   MPI_Finalize(); return 0;
+  }
+
+  m = atol(argv[1]);
   int message_out = rank;
   int message_in = -1;
   tag = 99;
-  int i; int n = 6;
-  if (rank == 0 ) message_in = 333;
+  int i,k; int n = 6;
+
+  double  time1,time2;
+  time1 = MPI_Wtime();
+
+  if (rank == 0 ) message_in = 0 ;
+  for ( k = 0; k < m; k++){
+ 
   for (i=0;i<n; i++){
   if(rank % n == i)
   {
     destination = (rank +1) %n;
-    message_out = message_in;
+    message_out = message_in + rank;
     MPI_Send(&message_out, 1, MPI_INT, destination, tag, MPI_COMM_WORLD);
   }
-  else if (rank % n == i+1)
+  else if ((rank+n-1) % n == i)
   {
     source = (rank+n -1)%n ;
 
     MPI_Recv(&message_in,  1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
   }
   }
-  printf("rank %d hosted on %s received from %d the message %d\n", rank, hostname, source, message_in);
+//  printf("%d rounds, rank %d received from %d the message %d \n",k, rank, source, message_in);
+  }
+  time2 = MPI_Wtime();
+  double elapsed = time2 - time1;
+  printf("%d rounds, rank %d, communication time %f \n ",k, rank, elapsed);
 
   MPI_Finalize();
   return 0;
