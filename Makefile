04d3b5e97ef57c570d36e8478f7b6342688e677c
Kan Bok-Hua <mhc431@nyu.edu>
2017-03-30 15:37:17 -0400

    add int_ring_2mb

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 CC=mpicc
 FLAGS=-O3
-EXECS= int_ring
+EXECS= int_ring int_ring_2mb
 
 all: ${EXECS}
 
 int_ring: int_ring.c
 	${CC} ${FLAGS} $^ -o int_ring
+int_ring_2mb : int_ring_2mb.c
+	${CC} ${FLAGS} $^ -o int_ring_2mb
 
 clean:
 	rm -f ${EXECS}
---
+++ int_ring_2mb.c
@@ -0,0 +1,60 @@
+/* Communication ping-pong:
+ * Exchange between messages between mpirank
+ * 0 <-> 1, 2 <-> 3, ....
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <mpi.h>
+
+int main( int argc, char *argv[])
+{
+  int rank, tag, source, destination, m, n;
+  MPI_Status status;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &n);
+
+  if (argc != 2) {
+    fprintf(stderr, "Function needs the number of passes as input arguments!\n");
+   MPI_Finalize(); return 0;
+  }
+
+  m = atol(argv[1]);
+  int message_out = rank;
+  int message_in = -1;
+  tag = 99;
+  int i,k, length = 500000;
+  int data[500000];
+
+  double  time1,time2;
+  time1 = MPI_Wtime();
+
+  if (rank == 0 ) message_in = 0 ;
+  for ( k = 0; k < m; k++){
+ 
+  for (i=0;i<n; i++){
+  if(rank % n == i)
+  {
+    destination = (rank +1) %n;
+//    message_out = message_in + rank;
+    MPI_Send(&data, length, MPI_INT, destination, tag, MPI_COMM_WORLD);
+//    MPI_Send(&message_out, 1, MPI_INT, destination, tag, MPI_COMM_WORLD);
+  }
+  else if ((rank+n-1) % n == i)
+  {
+    source = (rank+n -1)%n ;
+
+//    MPI_Recv(&message_in,  1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(&data,  length, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+  }
+  }
+  printf("%d rounds, rank %d received from %d the message %d \n",k, rank, source, message_in);
+  }
+  time2 = MPI_Wtime();
+  double elapsed = time2 - time1;
+//  printf("%d rounds, rank %d, communication time %f \n sec. ",k, rank, elapsed);
+
+  MPI_Finalize();
+  return 0;
+}
